2491309999672a54799327b6180b0fc6fb8e60d0
shiva bhandari <malleshbhandari018@gmail.com>
2026-05-30 15:30:05 +0530

    added basic addition logic using functions

---
+++ Addition.py
@@ -0,0 +1,9 @@
+def Addition(first_value,second_value):              # use method , and two parameters
+    return first_value + second_value                # logic , return after Addition 
+
+num1 = int(input("Enter First Value: "))             #  first variable  declared 
+
+num2 = int(input("Enter Second Value: "))            # second  variable declared 
+
+print(Addition(num1,num2))                           # use print() to display output 
+
