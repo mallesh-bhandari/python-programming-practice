9480982ccbcfe3f47f881fd8e44a952fdd07aea8
shiva bhandari <malleshbhandari018@gmail.com>
2026-05-30 22:35:14 +0530

    Print Only Even  Numbers Programs

---
+++ Print_Even_Numbers.py
@@ -0,0 +1,11 @@
+# Use Function
+def Even_Numbers(number):
+# Use For Loop    
+    for i in range(2,number+1, 2):
+        print(i)
+        
+# User Input 
+Num = int(input("Enter Number "))
+
+# Output
+Even_Numbers(Num)
