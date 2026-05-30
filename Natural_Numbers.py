d51d5977cb4f925c33c106c1597fa7db2787d1a9
shiva bhandari <malleshbhandari018@gmail.com>
2026-05-30 16:16:19 +0530

    Implement natural numbers generator with user input

---
+++ Natural_Numbers.py
@@ -0,0 +1,8 @@
+def Natural_Numbers(numbers):                      # Declared function 
+    for i in range(1,numbers+1):                   # Use for Loop 
+        print(i)
+        
+Value = int(input("Enter The Number: "))           # Use User input 
+
+Natural_Numbers(Value)                             # function call 
+
