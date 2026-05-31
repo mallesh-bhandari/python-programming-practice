a9985fc8f7c55548f088e3cdf06e1ab2838ba273
shiva bhandari <malleshbhandari018@gmail.com>
2026-05-31 08:04:07 +0530

    Print Only Natural Numbers Program

---
+++ Natural_Numbers.py
@@ -0,0 +1,11 @@
+def natural_numbers(numbers):
+# Use  For Loop     
+    for i in range(1, numbers + 1):
+        print(i)
+
+
+# User Input 
+value = int(input("Enter The Number: "))
+
+# Output
+natural_numbers(value)
