b880d55e72e8a3ae52d30000e18c15caeae17ac6
shiva bhandari <malleshbhandari018@gmail.com>
2026-05-31 08:16:48 +0530

    Check Number Is Even Or Odd Program

---
+++ Even_Odd.py
@@ -0,0 +1,12 @@
+def check_even_odd(value):
+# Even logic     
+    if value % 2 == 0:  
+        print("This is an Even Number")
+    else:
+        print("This is an Odd Number")
+
+# User input
+num = int(input("Enter The Number: "))  
+
+# Output
+check_even_odd(num)  
