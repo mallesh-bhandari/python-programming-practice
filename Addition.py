a5191311dd2f98c7dba8dcfa59966f188a15b060
shiva bhandari <malleshbhandari018@gmail.com>
2026-05-31 08:23:11 +0530

    Addition Of Two Numbers Program

---
+++ Addition.py
@@ -0,0 +1,10 @@
+def Addition(first_value, second_value):
+#  Return     
+    return first_value + second_value  
+
+# First User Input 
+num1 = int(input("Enter First Value: ")) 
+# Second User Input
+num2 = int(input("Enter Second Value: "))  
+# output
+print(Addition(num1, num2))  
