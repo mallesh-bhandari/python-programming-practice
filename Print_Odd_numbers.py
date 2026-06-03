e4fa2281c1b995dea6c29624e3bb97f882db22a1
shiva bhandari <malleshbhandari018@gmail.com>
2026-06-03 13:09:50 +0530

    Print Only Odd Numbers

---
+++ Print_Odd_numbers.py
@@ -0,0 +1,10 @@
+def Odd_Numbers(Numbers):
+# Loop Used    
+    for i in range(1,Numbers+1,2):
+# Display Output        
+        print(i)   
+        
+        
+Numbers = int(input("Enter Any number: "))
+
+Odd_Numbers(Numbers)        
